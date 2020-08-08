e74dad1f34ddf10ba41a654d74c0f22d5b0eb188
ori.nachum <ori.nachum@tipalti.com>
2020-08-08 07:35:19 +0300

    Added framework and test for ThermalNotifier

---
+++ TemperatureNotifierTests/Framework/MockHttpMessageHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TemperatureNotifierTests.Framework
+{
+    public class MockHttpMessageHandler : HttpMessageHandler
+    {
+        private Queue<(string response, HttpStatusCode statusCode)> _responseQueue;
+
+        public string Input { get; private set; }
+        public int NumberOfCalls { get; private set; }
+
+
+        public MockHttpMessageHandler()
+        {
+            _responseQueue = new Queue<(string response, HttpStatusCode statusCode)>();
+        }
+
+        public void QueueNextResponse(string response, HttpStatusCode statusCode)
+        {
+            _responseQueue.Enqueue((response, statusCode));
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            NumberOfCalls++;
+            if (request.Content != null) // Could be a GET-request without a body
+            {
+                Input = await request.Content.ReadAsStringAsync();
+            }
+
+            var (response, statusCode) = _responseQueue.Dequeue();
+            return new HttpResponseMessage
+            {
+                StatusCode = statusCode,
+                Content = new StringContent(response)
+            };
+        }
+    }
+
+}
---
+++ TemperatureNotifierTests/ThermalNotifierServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TemperatureNotifierTests.Framework;
+using ThermalNotifierWS.Service;
+using Xunit;
+
+namespace TemperatureNotifierTests
+{
+    public class ThermalNotifierServiceTest
+    {
+        private ILogger _logger;
+        private ThermalNotifierService _thermalNotifierService;
+        private MockHttpMessageHandler _mockHttpMessageHandler;
+
+        public ThermalNotifierServiceTest()
+        {
+            _mockHttpMessageHandler = new MockHttpMessageHandler();
+            var httpClient = new HttpClient(_mockHttpMessageHandler);
+
+            _logger = A.Fake<ILogger>();
+            _thermalNotifierService = new ThermalNotifierService(httpClient, _logger);
+        }
+
+        [Fact]
+        public async Task NotifyTest()
+        {
+            _mockHttpMessageHandler.QueueNextResponse("32.45", HttpStatusCode.OK);
+            await _thermalNotifierService.NotifyTemperatureAsync();
+
+            Assert.Equal(2, _mockHttpMessageHandler.NumberOfCalls);
+        }
+    }
+}
