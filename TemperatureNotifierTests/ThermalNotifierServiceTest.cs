940c1df9dbf5fdf95c2e56ddb7978b4d6e9889d7
ori.nachum <ori.nachum@tipalti.com>
2020-08-08 07:45:02 +0300

    Added testing of failure

--- TemperatureNotifierTests/ThermalNotifierServiceTest.cs
+++ TemperatureNotifierTests/ThermalNotifierServiceTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -27,13 +28,23 @@ namespace TemperatureNotifierTests
             _thermalNotifierService = new ThermalNotifierService(httpClient, _logger);
         }
 
-        [Fact]
-        public async Task NotifyTest()
+        [Theory]
+        [InlineData(0, 1, false)]
+        [InlineData(1, 2, false)]
+        [InlineData(2, 2, true)]
+        public async Task NotifyTemperature_OkResponses_CallsAtleastOneMoreAndTrueOnSuccess(int numberOfOKResponses, int numberOfCalls, bool result)
         {
-            _mockHttpMessageHandler.QueueNextResponse("32.45", HttpStatusCode.OK);
-            await _thermalNotifierService.NotifyTemperatureAsync();
-
-            Assert.Equal(2, _mockHttpMessageHandler.NumberOfCalls);
+            foreach (int index in Enumerable.Range(0, numberOfOKResponses))
+            {
+                _mockHttpMessageHandler.QueueNextResponse("32.45", HttpStatusCode.OK);
+            }
+            foreach (int index in Enumerable.Range(numberOfOKResponses, 2))
+            {
+                _mockHttpMessageHandler.QueueNextResponse("32.45", HttpStatusCode.NotFound);
+            }
+            bool actualResult = await _thermalNotifierService.NotifyTemperatureAsync();
+            Assert.Equal(numberOfCalls, _mockHttpMessageHandler.NumberOfCalls);
+            Assert.Equal(result, actualResult);
         }
     }
 }
